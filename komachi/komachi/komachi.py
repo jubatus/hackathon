800461bba6f854c7ac7b271c6592ff7b621c662f
Tetsuya SHIODA <shioda.tetsuya@lab.ntt.co.jp>
2016-11-12 18:27:43 +0900

    remove unused print message

--- komachi/komachi/komachi.py
+++ komachi/komachi/komachi.py
@@ -41,7 +41,6 @@ def __html(url):
 
 def parse_titles_in_day(year, month, day, page_id=1):
     url = '{0}/d/?d={1:02d}{2:02d}{3:02d}&p={4}'.format(BASE_URL, year, month, day, page_id)
-    print(url)
     soup = BeautifulSoup(__html(url), 'lxml')
     topics_list = soup.find('table', class_='topicslist').find_all('tr')[1:]
     result = []
